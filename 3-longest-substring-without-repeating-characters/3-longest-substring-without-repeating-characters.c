a1d838859627ed667bedee3c8f69b0f050901320
Muhmmad magdy srour <mosmagdysror@live.com>
2022-05-20 14:02:36 +0200

    Time: 8 ms (62.27%), Space: 5.9 MB (42.68%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.c
@@ -0,0 +1,43 @@
+#include<stdlib.h>
+
+int lengthOfLongestSubstring(char * s){
+    int retLongestSubString = 0;
+    int maxLongStr = 0;
+    int newStrStartIdx = 0;
+    int i = 0,j = 0;
+    while(s[i] != '\0')
+    {
+        // printf("current char:%c ",s[i]);
+        /*current Idx is one or more steps ahead new string start Idx*/
+        if(newStrStartIdx < i)
+        {
+            maxLongStr = i - newStrStartIdx + 1;
+            for(j = newStrStartIdx; j < i;j++)
+            {
+                // printf(" | checked char:%c ",s[j]);
+                /*we found a dublicated char so, make new string from this point*/
+                if(s[i] == s[j])
+                {
+                    newStrStartIdx = j+1;
+                    maxLongStr = 0;
+                    // printf("*");
+                    break;
+                }
+            }
+        }
+        /*current Idx is the start Idx*/
+        else
+        {
+            maxLongStr = 1;
+        }
+        /*new longer string*/
+        if(maxLongStr > retLongestSubString)
+        {
+            retLongestSubString = maxLongStr;
+        }
+        /*increment Idx*/
+        i++;
+        // printf("\n");
+    }
+    return retLongestSubString;
+}
