5b27164eca51c5c017a3513e3e1df22f4cd4b7bb
Muhmmad magdy srour <mosmagdysror@live.com>
2022-06-03 20:33:25 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (5.93%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.c
+++ 63-unique-paths-ii/63-unique-paths-ii.c
@@ -16,30 +16,27 @@ int uniquePathsWithObstacles(int ** obstacleGrid, int obstacleGridSize, int* obs
     int row,col;
     int upNodeNumOfPaths = 0,leftNodeNumOfPaths = 0;
     int retNumOfPaths;
-    int * numOfPathsArray = (int *) malloc(sizeof(int) * obstacleGridSize * (*obstacleGridColSize));
-    //printf("pointer: %p\n",numOfPathsArray);
-    //printf("%d %d\n",obstacleGridSize,*obstacleGridColSize);
+    int numOfRows = obstacleGridSize,numOfCol = *obstacleGridColSize;
+    int * numOfPathsArray = (int *) malloc(sizeof(int) * numOfRows * numOfCol);
     /*iterate obstacleGrid*/
-    for(row = 0;row < obstacleGridSize;row++)
+    for(row = 0;row < numOfRows;row++)
     {
-        for(col = 0;col < *obstacleGridColSize;col++)
+        for(col = 0;col < numOfCol;col++)
         {
-            //printf("obstacleGrid[%d][%d]:%d - ",row,col,obstacleGrid[row][col]);
             /*check current node not to be an obstacle*/
             if(obstacleGrid[row][col] != 1)
             {
                 /*initialize arr[0][0] node*/
                 if((row == 0) && (col == 0))
                 {
-                    *(numOfPathsArray + (row * (*obstacleGridColSize)) + col)  = 1;
+                    numOfPathsArray [row * numOfCol + col]  = 1;
                 }
                 else
                 {
                     /*check up node to be a valid path*/
                     if((row-1 >= 0) && (obstacleGrid[row-1][col] != 1))
                     {
-                        upNodeNumOfPaths = *(numOfPathsArray + ((row-1) * (*obstacleGridColSize)) + col);
-                        // printf("(numOfPathsArray[%d][%d]:%d)",row-1,col,*(numOfPathsArray + ((row-1) * (*obstacleGridColSize)) + col));
+                        upNodeNumOfPaths = numOfPathsArray [(row-1) * numOfCol + col];
                     }
                     else
                     {
@@ -48,26 +45,24 @@ int uniquePathsWithObstacles(int ** obstacleGrid, int obstacleGridSize, int* obs
                     /*check left node to be a valid path*/
                     if((col-1 >= 0) && (obstacleGrid[row][col-1] != 1))
                     {
-                        leftNodeNumOfPaths = *(numOfPathsArray + (row * (*obstacleGridColSize)) + col-1);
+                        leftNodeNumOfPaths = numOfPathsArray [row * numOfCol + (col-1)];
                     }
                     else
                     {
                         leftNodeNumOfPaths = 0;
                     }
                     /*put the sum of up and left nodes paths into current node (if up or left nodes are obstacle or out of range assume to be 0)*/
-                    *(numOfPathsArray + (row * (*obstacleGridColSize)) + col)  = upNodeNumOfPaths + leftNodeNumOfPaths;
+                    numOfPathsArray [row * numOfCol + col] = upNodeNumOfPaths + leftNodeNumOfPaths;
                 }
             }
             else
             {
                 /*do nothing current node is an obstacle*/
-                *(numOfPathsArray + (row * (*obstacleGridColSize)) + col) = 0;
+                numOfPathsArray [row * numOfCol + col] = 0;
             }
-            //printf("numOfPathsArray[%d][%d]:%d |",row,col,(*(numOfPathsArray + (row * (*obstacleGridColSize)) + col)));
         }
-        //printf("\n");
     }
-    retNumOfPaths = *(numOfPathsArray +  (obstacleGridSize * (*obstacleGridColSize)) - 1);
+    retNumOfPaths = numOfPathsArray [numOfRows * numOfCol - 1];
     free(numOfPathsArray);
     return retNumOfPaths;
 }
