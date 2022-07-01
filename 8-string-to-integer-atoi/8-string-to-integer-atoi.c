549983f7d7a3cd56f10565e7cf414f2eca6527fa
Muhmmad magdy srour <mosmagdysror@live.com>
2022-07-01 14:52:28 +0200

    Time: 4 ms (57.51%), Space: 5.6 MB (56.35%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.c
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.c
@@ -1,74 +1,65 @@
 #define UINT_MAX (2147483647UL)
-int myAtoi(char * s){
-    signed int retVar = 0;
-    unsigned long int number = 0;
-    int i = 0;
-    signed char numSign = 1;
-    unsigned numStarted = 0;
-    while(s[i] != '\0')
-    {
-        /*get the number*/
-        if((s[i] >= '0') && (s[i] <= '9'))
-        {
-            numStarted = 1;
-            /*number * 10 -> add new decimal place, num - 48 because the asci representaion of '0' is 48 in decimal*/
-            number = (number*10) + (s[i] - 48);
-            /*check for overflow*/
-            if ((numSign > 0) && (number > UINT_MAX))
-            {
-                break;
-                
-            }
-            else if((numSign < 0) && (number > UINT_MAX+1))
-            {
-                break;
-            }
-        }
-        else if(numStarted != 1)
-        {
-            /*ignore white space*/
-            if(s[i] == ' ')
-            {
-                /**do nothing - ignore white space at start*/
-            }
-            /*check for negative sign*/
-            else if (s[i] == '-')
-            {
-                numSign = -1;
-                numStarted = 1;
-            }
-            /*check for positive sign*/
-            else if(s[i] == '+')
-            {
-                numSign = 1;
-                numStarted = 1;
-            }
-            else
-            {
-                break;
-            }
-        }
-        /*no new numbers to parse*/
-        else 
-        {
-            break;
-        }
-        i++;
+/**
+ * @brief function to convert string number into sint32
+ * @details
+ *          -1- iterate input string to get each char
+ *          -2- check if the current char is a number then parse this number and
+ * add to the overall number [check for var overflow] -3- if number did not
+ * start before so ignore white spaces and parse sign -4- anythnig else would
+ * not be valid so ignore -5- check boundaries and put sign
+ * @param s string input
+ * @return int
+ */
+int myAtoi(char* s) {
+  signed int retVar = 0;
+  unsigned long int number = 0;
+  int i = 0;
+  signed char numSign = 1;
+  unsigned numStarted = 0;
+  while (s[i] != '\0') {
+    /*get the number*/
+    if ((s[i] >= '0') && (s[i] <= '9')) {
+      numStarted = 1;
+      /*number * 10 -> add new decimal place, (num - 48) because the asci
+       * representaion of '0' is 48 in decimal*/
+      number = (number * 10) + (s[i] - 48);
+      /*check for overflow*/
+      if (number > UINT_MAX) {
+        break;
+      }
+    } else if (numStarted != 1) {
+      /*ignore white space*/
+      if (s[i] == ' ') {
+        /*do nothing - ignore white space at start*/
+      }
+      /*check for negative sign*/
+      else if (s[i] == '-') {
+        numSign = -1;
+        numStarted = 1;
+      }
+      /*check for positive sign*/
+      else if (s[i] == '+') {
+        numSign = 1;
+        numStarted = 1;
+      } else {
+        break;
+      }
     }
-    /*put sign*/
-    if ((numSign > 0) && (number > UINT_MAX))
-    {
-        retVar = UINT_MAX;
-        
+    /*no new numbers to parse*/
+    else {
+      break;
     }
-    else if((numSign < 0) && (number > UINT_MAX+1))
-    {
-        retVar = -(UINT_MAX+1);
-    }
-    else
-    {
-        /*put the sign*/
-        retVar = numSign * number;
-    }
-    return retVar;
+    i++;
+  }
+  /*put sign*/
+  if ((numSign > 0) && (number > UINT_MAX)) {
+    retVar = UINT_MAX;
+
+  } else if ((numSign < 0) && (number > UINT_MAX + 1)) {
+    retVar = -(UINT_MAX + 1);
+  } else {
+    /*put the sign*/
+    retVar = numSign * number;
+  }
+  return retVar;
 }
